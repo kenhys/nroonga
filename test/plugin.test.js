19fab25ebf063c8125b1337bec712c4527b10237
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-24 14:56:50 +0900

    appveyor: test extended path prefix \\?\
    There is a case that extended path prefix is not handled correctly
    in Groonga. It causes a bug that Groonga's plugin can't be found.

---
+++ test/plugin.test.js
@@ -0,0 +1,48 @@
+/* eslint no-unused-expressions: 0 */
+'use strict'
+
+const fs = require('fs-extra')
+const path = require('path')
+const expect = require('chai').expect
+const nroonga = require('../lib/nroonga')
+
+/* global beforeEach, afterEach, describe, it */
+describe('plugin', () => {
+  let db = null
+  const tempdir = path.join('test', 'tmp')
+  const databaseName = `tempdb-${process.pid}-${(new Date()).valueOf()}`
+
+  beforeEach(() => {
+    if (!fs.existsSync(tempdir)) fs.mkdirSync(tempdir)
+    db = new nroonga.Database(path.join(tempdir, databaseName))
+  })
+
+  afterEach(() => {
+    db.close()
+    fs.removeSync(tempdir)
+  })
+
+  describe('TokenMecab', () => {
+    it('should be registered', () => {
+      const matched = db.commandSync('plugin_register tokenizers/mecab')
+      expect(matched).to.equal(true)
+    })
+
+    it('should tokenize tokyo to', () => {
+      const matched = db.commandSync('tokenize TokenMecab "東京都"')
+      const expected = [{
+        value: '東京',
+        position: 0,
+        force_prefix: false,
+        force_prefix_search: false
+      },
+      {
+        value: '都',
+        position: 1,
+        force_prefix: false,
+        force_prefix_search: false
+      }]
+      expect(matched).to.deep.equal(expected)
+    })
+  })
+})
